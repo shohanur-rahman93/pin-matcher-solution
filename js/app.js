3770cd5eba33046b692681526440b11e801eb10a
shohanurrahman9361 <shohanur.rahman.cse.bubt@gmail.com>
2023-09-30 22:46:52 +0600

    app js file initialized

---
+++ js/app.js
@@ -0,0 +1,16 @@
+function getPin(){
+    const pin = generatePin();
+    const pinString = pin + '';
+    if(pinString.length === 4){
+        return pin;
+    }
+    else{
+        console.log('pin not 3 digit found',pin);
+        return getPin();
+    }
+}
+
+function generatePin(){
+    const random = Math.round(Math.random()*10000);
+    return random;
+}
